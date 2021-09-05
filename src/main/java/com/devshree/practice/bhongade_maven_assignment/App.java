5f2f7eca5b772b8071f42a06f1c9b116b392ba00
devshree <devshree12bhongade@gmail.com>
2021-09-05 18:26:56 +0530

    Maven Eclipse Practise task

---
+++ src/main/java/com/devshree/practice/bhongade_maven_assignment/App.java
@@ -0,0 +1,13 @@
+package com.devshree.practice.bhongade_maven_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Devshree Bhongade, Welcome to Eclipse !" );
+    }
+}
